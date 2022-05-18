ff770f6fdd81b75a03dae25208eb613f3bd7b6e0
Gabriel de Souza <devgabrieldesouza@gmail.com>
2022-05-17 21:56:16 -0300

    Drag

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,15 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { useDrag } from 'react-dnd';
 
 import { Container, Label } from './styles'
 
-export default function Card() {
+export default function Card({data}) {
+	const [{isDragging}, dragRef] = useDrag({
+		type: 'CARD',
+		collect: monitor => ({
+			isDragging: monitor.isDragging(),
+		})
+	});
 	return (
-		<Container>
+		<Container ref = {dragRef} isDragging={isDragging}>
 			<header>
-				<Label color="#7159c1"></Label>
+				{data.labels.map(label => <Label key={label} color={label} />)}
 			</header>
-			<p>Fazer a migração completa de servidor</p>
-			<img src = "https://mario.wiki.gallery/images/8/81/NSMBU_Bubble_Baby_Yoshi_Artwork.png" alt =""/>
+			<p>{data.content}</p>
+			{data.user && <img src = {data.user} alt =""/>}
 		</Container>
-	)
+	);
 }
--- src/components/List/styles.js
+++ src/components/List/styles.js
@@ -3,7 +3,9 @@ import styled from 'styled-components';
 export const Container=styled.div`
 	padding:015px;
 	height: 100%;
-	flex:00320px;
+	flex:320px;
+	opacity: ${props => props.done ? 0.6 : 1};
+
 	&+div{
 		border-left: 1px solid rgba(0, 0, 0, 0.05);
 	}
