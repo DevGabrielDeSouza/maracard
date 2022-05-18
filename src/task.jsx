277c7916e56804ee627b5fec7492520de8453598
Gabriel de Souza <devgabrieldesouza@gmail.com>
2022-05-18 04:21:05 -0300

    Handle logic

--- src/task.jsx
+++ src/task.jsx
@@ -2,13 +2,22 @@ import React from 'react';
 import styled from 'styled-components';
 import { Draggable } from 'react-beautiful-dnd';
 
-const Container=styled.div`
+const Container = styled.div`
 	border: 1px solid lightgrey;
 	border-radius: 2px;
 	padding: 8px;
 	margin-bottom: 8px;
 	background-color: ${props => (props.isDragging ? 'lightgreen' : 'white')};
+	display: flex;
 `
+
+const Handle = styled.div`
+	width:20px;
+	height:20px;
+	background-color:orange;
+	border-radius:4px;
+	margin-right:8px;
+ `
 
 export default class Task extends React.Component {
 	render() {
@@ -17,10 +26,12 @@ export default class Task extends React.Component {
 				{(provided, snapshot) => (
 					<Container
 						{...provided.draggableProps}
+						//remove this line to to only allow grabbing from handle
 						{...provided.dragHandleProps}
 						ref={provided.innerRef}
 						isDragging={snapshot.isDragging}
 					>
+						<Handle {...provided.dragHandleProps} />
 						{this.props.task.content}
 					</Container>
 				)}
