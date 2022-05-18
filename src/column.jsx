b1c802140ba79366de28927ca632da3bb221be06
Gabriel de Souza <devgabrieldesouza@gmail.com>
2022-05-18 01:46:01 -0300

    Drag and Drop Tasks in Columns

--- src/column.jsx
+++ src/column.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Droppable } from 'react-beautiful-dnd';
 import Task from './task';
 
 const Container = styled.div`
-	margin:8px;
-	border:1px solid lightgrey;
-	border-radius:2px;
+	margin: 8px;
+	border: 1px solid lightgrey;
+	border-radius: 2px;
 `
 
 const Title = styled.h3`
@@ -20,7 +21,17 @@ export default class Column extends React.Component {
 		return(
 			<Container>
 				<Title>{this.props.column.title}</Title>
-				<TaskList>{this.props.tasks.map(task => <Task key={task.id} task ={task} />)}</TaskList>
+				<Droppable droppableId={this.props.column.id}>
+					{provided => (
+						<TaskList
+							ref={provided.innerRef}
+							{...provided.droppableProps}
+						>
+							{this.props.tasks.map((task, index) => <Task key={task.id} task={task} index={index}/>)}
+							{provided.placeholder}
+						</TaskList>
+					)}
+				</Droppable>
 			</Container>
 		)
 	}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import '@atlaskit/css-reset';
+import { DragDropContext } from 'react-beautiful-dnd';
 import initialData from './initial-data';
 import Column from './column';
 
@@ -21,14 +22,23 @@ root.render(
 class App extends React.Component {
 	state = initialData;
 
-	render() {
-		return this.state.columnOrder.map(columnId => {
-			const column = this.state.columns[columnId];
-			const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+	onDragEnd = result => {
 
-			//return column.title;
-			return <Column key={column.id} column={column} tasks={tasks} />;
-		});
+	};
+
+	render() {
+		return (
+			<DragDropContext onDragEnd={this.onDragEnd} >
+			{this.state.columnOrder.map(columnId => {
+				const column = this.state.columns[columnId];
+				const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+	
+				//return column.title;
+				return <Column key={column.id} column={column} tasks={tasks} />;
+			})}
+			</DragDropContext>
+
+		);
 	}
 }
 
