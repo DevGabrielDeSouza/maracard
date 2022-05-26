81968de3d86d5f92b714ffb25e6d50450b7527ee
Gabriel de Souza <devgabrieldesouza@gmail.com>
2022-05-26 07:56:11 -0300

    Basic Quill editor component

--- src/TextEditor.js
+++ src/TextEditor.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import { Quill } from 'quill';
+import React, { useCallback } from 'react';
+import * as Quill from "quill";
 import "quill/dist/quill.snow.css";
 
 // import { Container } from './styles';
 
+
 function TextEditor() {
-  return <div id="container">TextEditor</div>;
+
+	const wrapperRef = useCallback((wrapper) => {
+		if(wrapper == null) return;
+
+		wrapperRef.innerHTML = "";
+		const editor = document.createElement("div");
+		wrapper.append(editor);
+		new Quill(editor, { theme: 'snow' })
+	}, [])
+	return <div id="container" ref={wrapperRef}></div>;
 }
 
 export default TextEditor;
